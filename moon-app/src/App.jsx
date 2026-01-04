2440c194e23900445bb4d56d6f3c7d16457394b0
David <dbdavebrand@gmail.com>
2026-01-03 19:50:47 -0600

    corrected incongruent component styles

--- moon-app/src/App.jsx
+++ moon-app/src/App.jsx
@@ -7,6 +7,9 @@ import SkyDetails from "./SkyDetails.jsx";
 function App() {
   const [skyData, setSkyData] = useState(null);
 
+  const locationName = "Franklin, TN"; // Hardcoded for now
+  const coordinates = { lat: 35.9251, lon: -86.8689 }; // Hardcoded for now
+
   useEffect(() => {
     console.log("Attempting to fetch sky data..."); // DEBUG
     fetch("http://127.0.0.1:8000/sky-summary")
@@ -31,30 +34,16 @@ function App() {
         gap: "20px",
         flexWrap: "nowrap", // CRITICAL: Prevents stacking on mobile
         width: "100%", // Takes up full width
-        maxWidth: "1200px", // Keeps it from getting TOO wide on giant monitors
+        maxWidth: "1400px", // Keeps it from getting TOO wide on giant monitors
         overflowX: "auto", // Adds a scrollbar ONLY if the screen is tiny
         paddingBottom: "10px" // Space for the scrollbar if it appears
       }}
     >
-      <h1
-        style={{
-          color: "#5a5242ff",
-          marginBottom: "10px",
-          fontWeight: "200",
-          letterSpacing: "3px",
-          textAlign: "center",
-          width: "80%",
-          fontSize: "2.5rem",
-          fontFamily: "sans-serif",
-          textShadow: "0 0 10px rgba(255, 255, 255, 0.2)",
-          marginTop: "20px",
-          userSelect: "none", // prevent text selection
-          textTransform: "uppercase",
-          textShadow: "0 0 10px rgba(255, 255, 255, 0.3)"
-        }}
+      <h1 style={{ textAlign: "center", width: "100%", marginBottom: "20px", letterSpacing: "4px" }}
       >
-        SKY DASHBOARD
+        SKY DASHBOARD 
       </h1>
+      <p>for {locationName} ({coordinates.lat}° N, {coordinates.lon}° W)</p>
 
       <div
         style={{
@@ -64,7 +53,6 @@ function App() {
           justifyContent: "center",
           gap: "50px",
           flexWrap: "wrap",
-          fontStyle: "italic",
           padding: "20px",
           backgroundColor: "rgba(0, 0, 0, 0.5)",
           borderRadius: "20px",
