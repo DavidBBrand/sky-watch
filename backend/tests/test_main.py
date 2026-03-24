7a53ac7e0ca51184f03354faa4304ab5140073ec
David <dbdavebrand@gmail.com>
2026-03-24 13:43:36 -0500

    fix(ci): correct pytest logic

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -7,7 +7,7 @@ async def test_sky_summary_success():
     # New way to initialize the client for FastAPI
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        response = await ac.get("/sky-summary?lat=abc&lon=-86.86")
+        response = await ac.get("/sky-summary?lat=35.92&lon=-86.86")
     
     assert response.status_code == 200
     data = response.json()
@@ -18,5 +18,6 @@ async def test_sky_summary_success():
 async def test_invalid_coordinates():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        response = await ac.get("/sky-summary")
+        response = await ac.get("/sky-summary?lat=abc&lon=-86.86")
+    
     assert response.status_code == 422
