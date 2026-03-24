c04e5190c4a81335ca972b9dd49aded187697da6
David <dbdavebrand@gmail.com>
2026-03-24 13:36:50 -0500

    fix(ci): update yaml to include Redis

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -7,7 +7,7 @@ async def test_sky_summary_success():
     # New way to initialize the client for FastAPI
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        response = await ac.get("/sky-summary?lat=35.92&lon=-86.86")
+        response = await ac.get("/sky-summary?lat=abc&lon=-86.86")
     
     assert response.status_code == 200
     data = response.json()
