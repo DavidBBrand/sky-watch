d4bbf2645b341d9b68f02fe761c190c871070037
dbdav <dbdavebrand@gmail.com>
2026-09-21 15:37:13 -0500

    style(loading screen/copyright/main title): removed negative letter spacing from loading screen text, replaced click with select, and set copyright statement to Oxanium

--- skyapp-frontend/src/App.tsx
+++ skyapp-frontend/src/App.tsx
@@ -196,7 +196,7 @@ const App: React.FC = () => {
         <div className="scanner"></div>
         <div>Loading...</div>
         <div>
-          Click <strong>'Allow'</strong> for location access
+          Select '<strong>Allow</strong>' for location access
         </div>
       </div>
     );
